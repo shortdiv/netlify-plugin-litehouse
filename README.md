b4199bde13f7b64b805e727e62f67bd2f80ffc5e
erezrokah <erezrokah@users.noreply.github.com>
2020-05-20 20:19:13 +0300

    refactor: use puppeteer built in browser

--- README.md
+++ README.md
@@ -5,20 +5,21 @@ A Netlify plugin to generate a lighthouse report for every deploy
 > NOTICE: This is an experimental feature. Subject to lots of change.
 
 ## Usage
+
 This plugin can be included via npm. Install it as a dependency with the following command:
 
-```
+```bash
 npm install --save netlify-plugin-lighthouse
 ```
 
-To use a build plugin, create a `plugins` in your `netlify.yml` like so: 
+To use a build plugin, create a `plugins` in your `netlify.toml` like so:
 
-```yaml
-plugins:
-  - type: netlify-plugin-lighthouse
-    config:
-      url: "YOUR_CUSTOM_URL"
-    
+```toml
+[[plugins]]
+package = "netlify-plugin-lighthouse"
+  [plugins.inputs]
+  # optional, defaults to scanning the current built version of the site
+  audit_url = 'https://www.my-custom-site.com'
 ```
 
 Run `netlify build` locally to check that the plugin works
--- index.js
+++ index.js
@@ -1,38 +1,93 @@
-const fs = require('fs');
+const httpServer = require("http-server");
+const puppeteer = require("puppeteer");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
 
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
-
-const {
-  env: {
-    DEPLOY_URL
+const getServer = (url, serveDir) => {
+  if (url) {
+    // return a mock server for readability
+    const server = {
+      listen: async (func) => {
+        await func();
+      },
+      close: () => undefined,
+    };
+    return { server, url };
   }
-} = require('process')
+
+  if (!serveDir) {
+    throw new Error("Empty publish dir");
+  }
+
+  console.log(`Serving and scanning site from directory '${serveDir}'`);
+  const s = httpServer.createServer({ root: serveDir });
+  const port = 5000;
+  const host = "localhost";
+  const server = {
+    listen: (func) => s.listen(port, host, func),
+    close: () => s.close(),
+  };
+  return { url: `http://${host}:${port}`, server };
+};
 
 module.exports = {
-  name: 'netlify-plugin-lighthouse',
-  async onInit({ pluginConfig }) {
-    const opts = {
-      chromeFlags: []
-    };
-    const flags = {onlyCategories: ['performance']};
-    console.log(DEPLOY_URL)
-    function launchChromeAndRunLighthouse(url, opts, config = null) {
-       console.log("running")
-      return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
-        opts.port = chrome.port;
-        return lighthouse(url, opts, config).then(results => {
-          // const res=results.report;
-          // fs.writeFile("something.html", res, function(err) {
-          //   if(err) {
-          //       return console.log(err);
-          //   }
-          // });
-          return chrome.kill().then(() => results.lhr)
+  name: "netlify-plugin-lighthouse",
+  onSuccess: async ({
+    constants: { PUBLISH_DIR: serveDir = process.env.PUBLISH_DIR } = {},
+    utils,
+    inputs: { audit_url: auditUrl = process.env.AUDIT_URL } = {},
+  } = {}) => {
+    try {
+      utils = utils || {
+        build: {
+          failBuild: (message) => {
+            console.error(message);
+            process.exit(1);
+          },
+        },
+      };
+
+      const { server, url } = getServer(auditUrl, serveDir);
+      const browserFetcher = puppeteer.createBrowserFetcher();
+      const revisions = await browserFetcher.localRevisions();
+      if (revisions.length <= 0) {
+        throw new Error("Could not find local browser");
+      }
+      const info = await browserFetcher.revisionInfo(revisions[0]);
+      
+      const { error, results } = await new Promise((resolve) => {
+        server.listen(async () => {
+          let chrome;
+          try {
+            chrome = await chromeLauncher.launch({
+              chromePath: info.executablePath,
+              chromeFlags: ["--headless", "--no-sandbox", "--disable-gpu"],
+            });
+            const results = await lighthouse(url, {
+              port: chrome.port,
+            });
+            if (results.lhr.runtimeError) {
+              resolve({ error: new Error(results.lhr.runtimeError.message) });
+            }
+            resolve({ error: false, results });
+          } catch (error) {
+            resolve({ error });
+          } finally {
+            if (chrome) {
+              await chrome.kill().catch(() => undefined);
+            }
+            server.close()
+          }
         });
       });
+      if (error) {
+        throw error;
+      } else {
+        console.log(results);
+      }
+    } catch (error) {
+      console.error(`\nError: ${error.message}\n`);
+      utils.build.failBuild(`failed with error: ${error.message}`);
     }
-    const report = await launchChromeAndRunLighthouse(DEPLOY_URL || pluginConfig.url, flags)
-    console.log(report)
-  }
-}
+  },
+};
